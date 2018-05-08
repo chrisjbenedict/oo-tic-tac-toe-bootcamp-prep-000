16ccdf89c79a7ec337e76e31ce240669774e6da6
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-08 17:37:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,4 +68,23 @@ class TicTacToe
     turns
   end
 
+  def current_player
+    if turn_count(@board) %2 == 0
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combo|
+      if @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
+        return win_combo
+      elsif @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
+        return win_combo
+      end
+    end
+    false
+  end
+
 end
