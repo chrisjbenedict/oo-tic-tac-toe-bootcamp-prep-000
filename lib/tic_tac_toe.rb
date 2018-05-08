8cb3245434e164b85a4b356469b715dcbd94bf7d
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-08 17:23:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if !position_taken?(board, index) && index.between?(0,8)
+    if !position_taken?(index) && index.between?(0,8)
       true
     else
       false
